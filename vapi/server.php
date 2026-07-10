0e52688028f75183e5a27d5a9cd97f6a66e4e7bb
Stefano <1975236732@undc.edu.pe>
2026-07-10 01:03:17 -0500

    Actualización 3: Corregido API7, API8

--- vapi/server.php
+++ vapi/server.php
@@ -14,8 +14,17 @@ $uri = urldecode(
 // This file allows us to emulate Apache's "mod_rewrite" functionality from the
 // built-in PHP web server. This provides a convenient way to test a Laravel
 // application without having installed a "real" web server software here.
-if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
-    return false;
+if ($uri !== '/') {
+    $publicDir = realpath(__DIR__.'/public');
+    $requestedPath = realpath($publicDir.$uri);
+
+    if ($requestedPath !== false
+        && $publicDir !== false
+        && str_starts_with($requestedPath, $publicDir.DIRECTORY_SEPARATOR)
+        && is_file($requestedPath)
+    ) {
+        return false;
+    }
 }
 
 require_once __DIR__.'/public/index.php';
